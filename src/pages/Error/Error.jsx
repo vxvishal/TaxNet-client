e5bcab778224a1320228d7b51a2a0d6c7d36a5da
Vishal <singhvishalkumar997@gmail.com>
2023-10-12 08:31:30 +0530

    added error 404 page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Lottie from 'react-lottie-player';
+import Error404 from '../../assests/Error404.json';
+import styles from './Error.module.css';
 
 function Error() {
     return (
-        <div>Error 404</div>
+        <div className={styles.main}>
+            <Lottie
+                className={styles.lottie}
+                play
+                animationData={Error404}
+            />
+        </div>
     )
 }
 
